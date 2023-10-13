3efdef42a70b7f562cc66fe08fb78954c4d20401
Vincentime <vincentime147@gmail.com>
2023-10-13 17:41:31 +0200

    Clock : ok popup : start

--- js/listImg.js
+++ js/listImg.js
@@ -1,1 +1,5 @@
 const wallPaperList = ["show.png","1.jpg","2.jpg","3.jpg"];
+
+function returnList(){
+    return wallPaperList; 
+}
--- js/script.js
+++ js/script.js
@@ -1,20 +1,40 @@
  /* document.body.style.background = "#f3f3f3 url('img/show.png') no-repeat right top";
  */
+
+
+
+
 function changeWallPaper(){
     let a = Math.floor(Math.random() * 9);
     console.log(a);
-    document.body.style.background = `#f3f3f3 url('img/${a.toString()}.jpg') no-repeat right top`;
+    document.body.style.background = `#f3f3f3 url('img/${a.toString()}.jpg') no-repeat fixed center `;
+    document.body.style.backgroundSize = `auto 115%`;
+
 }
 changeWallPaper();
+setInterval(
+    function(){
+        var date = new Date();
+        var h = date.getHours();
+        var m = date.getMinutes();
+        var s = date.getSeconds() ;
+        if (h.toString().length== 1) h = `0${h.toString()}`;
+        if (m.toString().length== 1) m = `0${m.toString()}`;
+        if (s.toString().length== 1) s = `0${s.toString()}`;
+        /**
+         *document.getElementById("clock").innerHTML=date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
+         */
+        document.getElementById("clock").innerHTML=`${h}:${m}:${s}`;
+    },10
+);
 
 /*
+const wallPaperList = 
  document.write("avant")
 import wallPaperList from "listImg.js" ; 
 
 
 document.write(a);
-
-
 
 const fs = require('fs');
 document.write("tesssssssssssst");
